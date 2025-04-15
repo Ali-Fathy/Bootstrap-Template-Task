6d33763a11eb49aabdcc401117791ffb8721b520
Ali-Fathy <alifathy22a@gmail.com>
2025-04-15 02:10:42 +0200

    Updated Bootstrap setup and page text

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let menuBtn = document.querySelector(".menu-btn");
+
 menuBtn.addEventListener("click", () => {
-  document.getElementById("menuLinks").classList.toggle("show");
+  menuLinks.classList.toggle("show");
 });
 
-// Optional: click outside to close
 window.onclick = function (e) {
-  if (!e.target.matches(".menu-btn")) {
+  if (!e.target.matches(".menu-btn") && !e.target.matches(".icon")) {
     const menu = document.getElementById("menuLinks");
     if (menu.classList.contains("show")) {
       menu.classList.remove("show");
